998bdb0d125a738b46baf38441e4de825b9add8a
U-FRANCESCA\molt <Paul.Golds+normalvector@GMail.com>
2017-03-20 22:35:38 +0000

    Now get geometry for each section.

--- Source/mesh_deformer/DeformMeshComponent.cpp
+++ Source/mesh_deformer/DeformMeshComponent.cpp
@@ -2,6 +2,7 @@
 
 #include "mesh_deformer.h"
 #include "DeformMeshComponent.h"
+#include "KismetProceduralMeshLibrary.h"
 
 // Turn on/off debug options
 #define DEBUG_DEFORM_MESH_COMPONENT 1
@@ -27,11 +28,25 @@ void UDeformMeshComponent::Rebuild() {
 	// Clear the geometry
 	ProceduralMeshComponent->ClearAllMeshSections();
 
+	// These are the structures which will hold each mesh section
+	TArray<FVector> vertices;
+	TArray<int32> triangles;
+	TArray<FVector> normals;
+	TArray<FVector2D> uvs;
+	TArray<FProcMeshTangent> tangents;
+
 	// Iterate over the mesh sections
 	const int32 LOD = 0;
 	const int32 numSections = StaticMesh->GetNumSections(LOD);
 	for (int meshSectionIndex = 0; meshSectionIndex < numSections; ++meshSectionIndex) {
 		UE_LOG(LogTemp, Log, TEXT("DeformMeshComponent rebuilding section %d"), meshSectionIndex);
-
+		//  static void GetSectionFromStaticMesh(UStaticMesh* InMesh, int32 LODIndex, int32 SectionIndex, TArray<FVector>& Vertices, TArray<int32>& Triangles, TArray<FVector>& Normals,
+		// TArray<FVector2D>& UVs, TArray<FProcMeshTangent>& Tangents);
+		UKismetProceduralMeshLibrary::GetSectionFromStaticMesh(
+			StaticMesh, LOD, meshSectionIndex,
+			vertices, triangles, normals, uvs, tangents
+		);
+		UE_LOG(LogTemp, Log, TEXT("Found %d verts and %d triangles"), vertices.Num(), triangles.Num() / 3);
 	}
 }
+
